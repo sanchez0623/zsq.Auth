e51eb28714d3cb30da8396a12f6b1d3326ef3784
sanchez0623 <18576645357@163.com>
2018-12-15 22:58:40 +0800

    Use Cookie Authentication

---
+++ zsq.MvcCookieAuth/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using zsq.MvcCookieAuth.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+namespace zsq.MvcCookieAuth.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult MakeLogin()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,"sanchez"),
+                new Claim(ClaimTypes.Role,"admin")
+            };
+            var claimIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimIdentity));
+
+            return Ok();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return Ok();
+        }
+    }
+}
